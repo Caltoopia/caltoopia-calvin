3b179f5eee7ad4c92397b414ca8920301d1c5708
Per Persson <per.persson@ericsson.com>
2014-03-21 10:21:18 +0100

    Adding a very small example

---
+++ examples/loadableActors/minimal.cs
@@ -0,0 +1,24 @@
+# Minimal example to load actor classes, instantiate actors, and set up
+# a network
+
+# Load classes
+LOAD ./Example__m1.so
+
+# Create instances with parameters
+NEW art_Source_txt src fileName=indata.txt
+NEW art_Sink_txt snk1 fileName=outdata1.txt
+
+# Create instances without parameters
+NEW Example_m1 m1
+
+# Connect the source to multipliers
+CONNECT src.Out m1.In
+
+# Connect multipliers to separate sinks
+CONNECT m1.Out snk1.In
+
+# Enable actors
+ENABLE src snk1 m1
+
+# Execute the application
+# JOIN
