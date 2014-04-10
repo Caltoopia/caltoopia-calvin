e9adbad26a72b6b1485dd915379aae4da5a171fa
Per Persson <per.persson@ericsson.com>
2014-04-10 14:35:45 +0200

    For some reason we end up here with an uninitialized tokenBuffer. Breakpoint in sender_constructor is never hit. I can’t reproduce the problem on Master branch, so it’s likely caused by my changes, just not in an obvious way.

--- actors-teleport.c
+++ actors-teleport.c
@@ -602,6 +602,7 @@ ART_ACTION_SCHEDULER(sender_action_scheduler)
     {
       pthread_mutex_lock(&instance->tokenMon.lock);
       if (! instance->tokenMon.full) {
+        assert(instance->tokenMon.tokenBuffer);
         pinRead_dyn(input, instance->tokenMon.tokenBuffer, tokenSize);
         instance->tokenMon.full = 1;
         pthread_cond_signal(&instance->tokenMon.available);
