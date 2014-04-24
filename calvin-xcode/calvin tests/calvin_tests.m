56031ead98c72e05d6870d2eea449b4585db9b16
Per Persson <per.persson@ericsson.com>
2014-04-24 17:46:37 +0200

    Unit tests for the serialization.

---
+++ calvin-xcode/calvin tests/calvin_tests.m
@@ -0,0 +1,34 @@
+//
+//  calvin_tests.m
+//  calvin tests
+//
+//  Created by Per Persson on 2014-04-24.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface calvin_tests : XCTestCase
+
+@end
+
+@implementation calvin_tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    // XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
---
+++ calvin-xcode/calvin tests/coder_tests.m
@@ -0,0 +1,70 @@
+//
+//  coder_tests.m
+//  Calvin
+//
+//  Created by Per Persson on 2014-04-24.
+//
+//
+
+#import <XCTest/XCTest.h>
+#import "actors-coder.h"
+
+
+@interface coder_tests : XCTestCase
+{
+  ActorCoder *coder;
+  ActorCoder *decoder;
+}
+@end
+
+@implementation coder_tests
+
+- (void)setUp
+{
+  [super setUp];
+  coder = newCoder(JSON_CODER);
+  decoder = newCoder(JSON_CODER);
+}
+
+- (void)tearDown
+{
+  destroyCoder(coder);
+  destroyCoder(decoder);
+  [super tearDown];
+}
+
+- (void)testIntTypeEncodeDecode
+{
+  int original = 732654;
+  int target = 0;
+  const char *type = "i";
+
+  XCTAssertNotEqual(target, original, "Bad setup");
+  
+  CoderState *state = coder->init(coder);
+
+  coder->encode(state, "key", &original, type);
+  coder->decode(state, "key", &target, type);
+
+  XCTAssertEqual(target, original, "Coding of type \"%s\"failed", type);
+ }
+
+- (void)testStringTypeEncodeDecode
+{
+  const char *original = "hello";
+  char *target = NULL;
+  const char *type = "s";
+  
+  XCTAssertEqual(target, NULL, "Bad setup");
+  XCTAssertNotEqual(original, NULL, "Bad setup");
+  
+  CoderState *state = coder->init(coder);
+  
+  coder->encode(state, "key", &original, type);
+  coder->decode(state, "key", &target, type);
+
+  XCTAssertEqual(strncmp(target, original, strlen(original)), 0, "Coding of type \"%s\" failed", type);
+}
+
+
+@end
